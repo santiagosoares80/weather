e142eaa19d37e2d111d4ff13c7cf819479ce703f
Santiago Soares <santiagosoares@gmail.com>
2020-07-22 00:16:03 -0300

    Arduino script rewrite to use Virtual Wire

--- remote_arduino/remote_arduino.ino
+++ remote_arduino/remote_arduino.ino
@@ -1,44 +1,61 @@
 #include "DHT.h"
-#include <RH_ASK.h>
-#include <RHDatagram.h>
 #include <stdlib.h>
+#include <VirtualWire.h>
 
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11
 
-#define RXPIN 13
 #define TXPIN 12
 #define SPEED 2000
-#define MYADDRESS 0x0A
-#define SERVER 0x01
+#define MYADDRESS 0x0A //(ASCII code for A - 10)
+#define SERVER 0x01 //(ASCII code for 1)
 #define REBOOTMESSAGE "REBOOT"
-#define CLEARFLAG 0xFF
-#define CONTROLFLAG 0x10
-#define TEMPFLAG 0x01
-#define HUMIDFLAG 0x02
+#define CONTROLFLAG 0x10 //(ASCII code for A - 10) 
+#define TEMPFLAG 0x01 //(ASCII code for 1)
+#define HUMIDFLAG 0x02 //(ASCII code for 2)
+#define ID 0x01 //(ASCII code for 0)
 
 DHT dht(DHTPIN, DHTTYPE);
-RH_ASK radio(SPEED, RXPIN, TXPIN);
-RHDatagram manager(radio, MYADDRESS);
+
+void create_packet(char *message, char flags, char *payload) {
+  //Header of the packet has 4 bytes: to, from , id(not used) and flags
+  char header[5] = "";
+
+  // Creating header:
+  header[0] = SERVER;
+  header[1] = MYADDRESS;
+  header[2] = ID;
+  header[3] = flags;
+
+  //Insert header on packet
+  strcat(message, header);
+
+  //Insert payload on packet
+  strcat(message, payload);
+  
+}
 
 void setup() {
   Serial.begin(9600);
   Serial.println(F("Starting DHT11"));
   dht.begin();
 
-  Serial.println(F("Starting Radio Manager"));
-  if (!manager.init()) {
-    Serial.println(F("Failed to initilize manager!"));
-  }
+  Serial.println(F("Setting radio up"));
+  vw_set_tx_pin(TXPIN);
+  vw_setup(SPEED);
 
-  //Set flag to control message
-  manager.setHeaderFlags(CONTROLFLAG, CLEARFLAG);
+  //Create a packet
+  char packet[27] = ""; 
+  create_packet(packet, CONTROLFLAG, REBOOTMESSAGE);
+  Serial.println(F("Transmitting control packet:"));
+  Serial.write(packet, strlen(packet));
   
-  //Send alarm message informing reboot of the probe
-  if(!manager.sendto((uint8_t*)REBOOTMESSAGE, sizeof(REBOOTMESSAGE), SERVER)) {
-    Serial.println(F("Transmission of reboot message failed!"));
-    return;
-  }
+  //Send message
+  vw_send((uint8_t*)packet, strlen(packet));
+  vw_wait_tx();
+
+  //Clean packet
+  packet[0] = '\0';
 }
 
 void loop() {
@@ -47,6 +64,9 @@ void loop() {
   
   // Read humidity
   float h = dht.readHumidity();
+
+  //Wait to read again
+  delay(200);
   
   // Read temperature as Celsius
   float t = dht.readTemperature();
@@ -65,29 +85,36 @@ void loop() {
   Serial.println(F("°C "));
 
   // Convert floats to strings
-  char temp[5];
+  char temp[5] = "";
   dtostrf(t, 3, 1, temp);
-  char humidity[5];
+  char humidity[5] = "";
   dtostrf(h, 3, 1, humidity);
 
-  //Set flag to temperature message
-  manager.setHeaderFlags(TEMPFLAG, CLEARFLAG);
-  
-  // Try to transmit temperature
-  if(!manager.sendto((uint8_t*)temp, sizeof(temp), SERVER)) {
-    Serial.println(F("Transmission of temperature failed!"));
-    return;
-  }
-  
-  // Wait a little bit before transmiting humidity
+  //Create packet
+  char tpacket[27] = "";
+  create_packet(tpacket, TEMPFLAG, temp);
+  Serial.println(F("Transmitting tpacket:"));
+  Serial.write(tpacket, strlen(tpacket));
+  Serial.println("");
+  //Transmit packet 
+  vw_send((uint8_t *)tpacket, strlen(tpacket));
+  vw_wait_tx();
+
+  //Clear packet
+  //tpacket[0] = '\0';
+
   delay(1000);
 
-  //Set flag to humidity message
-  manager.setHeaderFlags(HUMIDFLAG, CLEARFLAG);
-  
-  //Try to transmit humidity
-  if(!manager.sendto((uint8_t*)humidity, sizeof(humidity), SERVER)) {
-    Serial.println(F("Transmission of humidity failed!"));
-    return;
-  }
+  //Create humidity packet
+  char hpacket[27] = "";
+  create_packet(hpacket, HUMIDFLAG, humidity);
+  Serial.println(F("Transmitting hpacket:"));
+  Serial.write(hpacket, strlen(hpacket));
+  Serial.println("");
+  //Transmit packet
+  vw_send((uint8_t *)hpacket, strlen(hpacket));
+  vw_wait_tx();
+
+  //Clear packet
+  //hpacket[0] = '\0';
 }
